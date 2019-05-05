fb34cd2327d850b8fa91c2393ae58cb9495c1773
Susannah Harris <harris.susannah@gmail.com>
2019-05-05 18:57:55 -0500

    First saves attempt struggling

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,1 +1,10 @@
-console.log( 'js' );
+let collection = [];
+
+function addToCollection( artist, title, year ){
+    {
+  collection.push( artist, title, year );
+  }
+  console.log( 'in my collection', collection );
+}
+
+addToCollection( 'Pulp', 'His and Hers', '1994');
