680bbe4ee6a4c318c0ca622be53134a63c8ee889
Susannah Harris <harris.susannah@gmail.com>
2019-05-05 19:51:28 -0500

    made some more edits added in push and made more variables with objects still not sure if I am doing this correctly

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,10 +1,13 @@
 let collection = [];
+let artist = {};
+let title = {};
+let year = {};
 
 function addToCollection( artist, title, year ){
-    {
-  collection.push( artist, title, year );
+  console.log( 'in my collection', artist, title, year );{
+    collection.push( artist, title, year);
+    }
   }
-  console.log( 'in my collection', collection );
-}
 
-addToCollection( 'Pulp', 'His and Hers', '1994');
+addToCollection( 'Pulp', 'His and Hers', 1994 );
+addToCollection( 'Metallica', 'Kill Em All', 1983 );
