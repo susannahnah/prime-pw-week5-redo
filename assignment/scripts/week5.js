f7d1b5e7e4ecdbe003b5a1ad81dad8e52d752f91
Susannah Harris <harris.susannah@gmail.com>
2019-05-05 20:10:07 -0500

    think I finally figured it out created new variable and assigned objects to it, pushed new variable into collections

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,12 +1,15 @@
 let collection = [];
-let artist = {};
-let title = {};
-let year = {};
 
 function addToCollection( artist, title, year ){
   console.log( 'in my collection', artist, title, year );{
-    collection.push( artist, title, year);
-    }
+    let newAlbum = {
+      artist: artist,
+      title: title,
+      year: year,
+    };
+    collection.push(newAlbum);
+    return;
+   }
   }
 
 addToCollection( 'Pulp', 'His and Hers', 1994 );
