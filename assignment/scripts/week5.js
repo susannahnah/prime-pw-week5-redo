0578a1e832481c70c62d3b1d893b6fbced6939d1
Susannah Fujimoto Harris <harris.susannah@gmail.com>
2019-11-14 15:36:09 -0600

    Update week5.js

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,1 +1,10 @@
+//create a variable "collection" that is an empty array
+//add a function to your script named addToCollection( title, artist, year) that, when called, creates a new record object and pushes it into the "collection" array
+//this function should return the newly created record object
 
+let collection = []; 
+
+  function addToCollection(){
+        console.log('in my collection, I have ', + artist );
+        
+    }
