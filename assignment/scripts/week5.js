57c77f683c11cb989f5283b2936815da2660121e
Susannah Fujimoto Harris <harris.susannah@gmail.com>
2019-11-14 15:33:16 -0600

    updated code - removed answers

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,16 +1,1 @@
-let collection = [];
 
-function addToCollection( artist, title, year ){
-  console.log( 'in my collection', artist, title, year );{
-    let newAlbum = {
-      artist: artist,
-      title: title,
-      year: year,
-    };
-    collection.push(newAlbum);
-    return newAlbum;
-   }
-  }
-
-console.log(addToCollection( 'Pulp', 'His and Hers', 1994 ));
-console.log(addToCollection( 'Metallica', 'Kill Em All', 1983 ));
